42641d8bc97f1323080f9b82d4ae045128a115a6
hmattosaffinitiv <hmattos@affinitiv.com>
2023-02-01 11:28:36 -0300

    rollback

---
+++ rollback.kts
@@ -0,0 +1,24 @@
+package _Self.buildTypes
+
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.FTPUpload
+import jetbrains.buildServer.configs.kotlin.buildSteps.ftpUpload
+
+object Rollback : BuildType({
+    name = "rollback"
+
+    steps {
+        ftpUpload {
+            name = "deploy backup"
+            targetUrl = "ftp://deploytest.somee.com/www.deploytest.somee.com"
+            securityMode = FTPUpload.SecurityMode.NONE
+            dataChannelProtection = FTPUpload.DataChannelProtectionMode.DISABLE
+            authMethod = usernameAndPassword {
+                username = "hebermattos"
+                password = "******"
+            }
+            transferMode = FTPUpload.TransferMode.AUTO
+            sourcePath = """C:\Users\HeberMattos\backup\www.deploytest.somee.com"""
+        }
+    }
+})

052921b1c4528d2ce9416796bef8e26b73cbeca4
hmattosaffinitiv <hmattos@affinitiv.com>
2023-02-08 09:41:03 -0300

    copy config prod

--- prod.kts
+++ prod.kts
@@ -4,15 +4,23 @@ import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.FTPUpload
 import jetbrains.buildServer.configs.kotlin.buildSteps.exec
 import jetbrains.buildServer.configs.kotlin.buildSteps.ftpUpload
+import jetbrains.buildServer.configs.kotlin.buildSteps.powerShell
 
 object ProdDeploy_2 : BuildType({
     name = "prod deploy"
+
     steps {
         exec {
             name = "backup"
             path = "%backupscript%"
             arguments = "%backupfolder%"
             param("script.content", """C:\Users\HeberMattos\backup\winscp-download.bat""")
+        }
+        powerShell {
+            name = "copy config files"
+            scriptMode = script {
+                content = """Copy-Item -Path %prodconfigfolder%\*.* -Destination %binfolder% -Recurse"""
+            }
         }
         ftpUpload {
             name = "deploy site 1"
@@ -38,4 +46,5 @@ object ProdDeploy_2 : BuildType({
             transferMode = FTPUpload.TransferMode.AUTO
             sourcePath = "%binfolder%"
         }
-    }})
+    }
+})
